b4d29443d55381b98b2d9bc6a3c347bbf4f574d5
Filip Talaga <talaga.filip@gmail.com>
2019-02-10 18:52:11 +0100

    Added canvas scalling

--- src/pure-canvas.jsx
+++ src/pure-canvas.jsx
@@ -2,15 +2,41 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class PureCanvas extends Component {
+    state = {
+        width: 180,
+        height: 320
+    }
+
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener('resize', this.updateDimensions);
+    }
+
+    updateDimensions = () => {
+        const height = window.innerHeight;
+        const width = window.innerWidth > 600 ? height / 16 * 9 : window.innerWidth;
+
+        this.setState({ width: width, height: height });
+        this.shouldUpdate = true;
+    }
+
     shouldComponentUpdate() {
+        if (this.shouldUpdate) {
+            this.shouldUpdate = false;
+            return true;
+        }
         return false;
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDimensions);
     }
 
     render() {
         return (
             <canvas
-                width="300"
-                height="300"
+                width={this.state.width}
+                height={this.state.height}
                 style={{ margin: 'auto' }}
                 ref={node => node ? this.props.contextRef(node.getContext('2d')) : null}
             />
