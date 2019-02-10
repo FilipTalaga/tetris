9bd447e60a465a9e64b19339d63cf67f61ff11ed
Filip Talaga <talaga.filip@gmail.com>
2019-02-10 16:36:45 +0100

    Bumped some colors

--- src/canvas.jsx
+++ src/canvas.jsx
@@ -20,7 +20,7 @@ class Canvas extends Component {
         ctx.clearRect(0, 0, width, height);
         ctx.translate(width / 2, height / 2);
         ctx.rotate((angle * Math.PI) / 180);
-        ctx.fillStyle = '#4397AC';
+        ctx.fillStyle = '#733C82';
         ctx.fillRect(-width / 4, -height / 4, width / 2, height / 2);
         ctx.restore();
     }
