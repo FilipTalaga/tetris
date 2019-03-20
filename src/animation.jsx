06e9f378b237999104360018f22b74a94ab89c33
Filip Talaga <talaga.filip@gmail.com>
2019-03-20 13:53:36 +0100

    Refactor

--- src/animation.jsx
+++ src/animation.jsx
@@ -2,32 +2,41 @@ import React, { Component } from 'react';
 import Canvas from './canvas';
 
 class Animation extends Component {
-    child = React.createRef();
-    lastRender = 0;
+    constructor(props) {
+        super(props);
+        this.child = React.createRef();
+        this.lastRender = 0;
+    }
 
     componentDidMount() {
-        document.addEventListener('keydown', this.handleKeyDown);
-        this.rAF = requestAnimationFrame(this.loop);
+        document.addEventListener('keydown', this.handleKeyDown.bind(this));
+        this.rAF = requestAnimationFrame(this.loop.bind(this));
     }
 
     componentWillUnmount() {
-        document.removeEventListener('keydown', this.handleKeyDown);
+        document.removeEventListener('keydown', this.handleKeyDown.bind(this));
         cancelAnimationFrame(this.rAF);
     }
 
-    handleKeyDown = e => {
+    handleKeyDown(e) {
         this.child.current.move(e.key);
     }
 
-    loop = timestamp => {
-        const progress = timestamp - this.lastRender;
-        // update every 200 ms
-        if (progress >= 200) {
+    loop(timestamp) {
+        if (timestamp - this.lastRender >= 200) { // update every 200 ms
+            this.updateLogic();
             this.lastRender = timestamp;
-            this.child.current.move('ArrowDown');
         }
+        this.updateGraphics();
+        this.rAF = requestAnimationFrame(this.loop.bind(this));
+    }
+
+    updateLogic() {
+        this.child.current.move('ArrowDown');
+    }
+
+    updateGraphics() {
         this.child.current.draw();
-        this.rAF = requestAnimationFrame(this.loop);
     }
 
     render = () => <Canvas ref={this.child} />;
--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Animation from './animation';
 
-class App extends Component {
-    render() {
-        return <Animation />;
-    }
-}
+const App = () => <Animation />;
 
 export default App;
