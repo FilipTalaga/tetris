08b250b06053eed8880a6ecd3672432a4d788cb2
Filip Talaga <talaga.filip@gmail.com>
2019-04-16 14:17:53 +0200

    Moved background divs into app component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import Game from './game';
 
-const App = () => <Game />;
+const App = () => (
+    <React.Fragment>
+        <div className="shape-container">
+            <div id="standard"></div>
+        </div>
+        <div className="shape-container">
+            <div id="light"></div>
+        </div>
+        <Game />
+    </React.Fragment>
+);
 
 export default App;
