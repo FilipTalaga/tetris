25c0ade78bc068a36049cda6ec97f47cf2fb9712
Filip Talaga <talaga.filip@gmail.com>
2019-04-16 14:38:14 +0200

    Replaced yarn with npm

--- webpack.config.js
+++ webpack.config.js
@@ -45,8 +45,6 @@ module.exports = {
             { from: 'public' }
         ]),
         new WorkboxPlugin.GenerateSW({
-            // these options encourage the ServiceWorkers to get in there fast
-            // and not allow any straggling "old" SWs to hang around
             clientsClaim: true,
             skipWaiting: true,
             navigateFallback: '/index.html'
